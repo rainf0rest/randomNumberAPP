ec74dc37ffe8a87e4c2a762ae51538f9297bdca3
rainf0rest <405763281@qq.com>
2016-06-12 23:33:07 +0800

    add the java_code

--- app/src/main/java/com/example/rain/randomnumber/MainActivity.java
+++ app/src/main/java/com/example/rain/randomnumber/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.rain.randomnumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    EditText begin, end;
+    com.gc.materialdesign.views.ButtonRectangle produce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        result = (TextView) findViewById(R.id.result);
+        begin = (EditText) findViewById(R.id.number1);
+        end = (EditText) findViewById(R.id.number2);
+        produce = (com.gc.materialdesign.views.ButtonRectangle) findViewById(R.id.producebtn);
+
+        produce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = Integer.parseInt(begin.getText().toString());
+                int b = Integer.parseInt(end.getText().toString());
+                result.setText("" + makeRandom(a, b));
+            }
+        });
+
+    }
+
+    int makeRandom(int a, int b) {
+        Random random = new Random();
+        return random.nextInt(b - a + 1) + a;
     }
 }
